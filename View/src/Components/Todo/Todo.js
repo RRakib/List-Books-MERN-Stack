b7d907f442d72b05cfc0626b75801d5a003827d3
Rakib Uddin <16103030@iubat.edu>
2019-02-23 20:36:02 +0600

    Added Concurrently for running both Server and Client at the same time

---
+++ View/src/Components/Todo/Todo.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"
+
+class Todo extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <b>Form</b>
+            </div>
+        )
+    }
+}
+
+export default Todo;
