831778c470a25655455b46dad8bbfdc2754375b1
Rakib Uddin <16103030@iubat.edu>
2019-03-01 21:12:18 +0600

    Preaparing for Deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let mongoose = require("mongoose");
 let mongoURI = require("./Config/keys");
+let path = require("path");
 let routeController = require("./Controller/routeController");
 
 let app = express();
@@ -21,6 +22,17 @@ mongoose.connect(mongoURI.mongoURI , {useNewUrlParser : true})
 
 // Setting Routes
 app.use("/api/books" , routeController);
+
+
+// Server static files in production
+if(process.env.NODE_ENV === "production"){
+    //Set static folder
+    app.use(express.static("View/build"));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname , "View" , "build" , "index.html"));
+    })
+}
 
 
 // Server Port
